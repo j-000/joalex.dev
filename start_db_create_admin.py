fa238eec5a81ce89d826202662ed7832acc8b75a
Joao Oliveira <joao00alex@gmail.com>
2021-04-18 18:53:11 +0100

    Bug

--- start_db_create_admin.py
+++ start_db_create_admin.py
@@ -11,10 +11,6 @@ def main(pwd):
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     User(name='Joao', surname='Oliveira', email='hello@joalex.dev', password=pwd, is_admin=True)
-    p1 = Pawn('R')
-    p1.add_message('ola')
-    p2 = Pawn('S')
-    p2.add_message('adeus sdsxx')
 
 
 if __name__ == '__main__':
