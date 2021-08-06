020090e0a863d498a5fb42495c75b715fc1d324d
Joao <joao00alex@gmail.com>
2021-08-06 22:05:12 +0100

    Angular

---
+++ joalex/src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from 'src/app/model/cards';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  card: Card;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ joalex/src/app/model/cards.ts
@@ -0,0 +1,5 @@
+export interface Card {
+  htmlText: string;
+  image: string;
+  cv?: boolean;
+}
