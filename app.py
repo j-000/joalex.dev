6a9919680551618d2cb38a310e9e8dc22a82a546
Joao <joao00alex@gmail.com>
2021-08-06 19:23:19 +0100

    Update

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
 import os
 import config
-from server import app
-from flask import (
-    render_template,
-    send_from_directory
-)
+from flask import Flask, send_from_directory
 
 
+def create_app():
+    app = Flask(__name__)
 
-if os.getenv('ENV') == 'production':
-    app.config.from_object(config.ProdConfig)
-elif os.getenv('ENV') == 'development':
-    app.config.from_object(config.DevConfig)
-else:
-    raise NotImplementedError('** ! ENV not set. **')
+    if os.getenv('ENV') == 'production':
+        app.config.from_object(config.ProdConfig)
+    elif os.getenv('ENV') == 'development':
+        app.config.from_object(config.DevConfig)
+    else:
+        raise NotImplementedError('** ! ENV not set. **')
+    return app
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+app = create_app()
 
 
 @app.route('/cv')
 def cv():
     return send_from_directory('static', 'my_cv_dev_joao.pdf', as_attachment=True)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
-
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
