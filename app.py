9ca7204532310929c98c16d20f323c34bc19334c
Joao Oliveira <joao00alex@gmail.com>
2021-11-26 10:09:59 +0000

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import config
 from flask import Flask, send_from_directory, render_template, request
-import json
+
 
 def create_app():
     app = Flask(__name__)
@@ -25,25 +25,6 @@ def home():
 @app.route('/cv')
 def cv():
     return send_from_directory('static', 'my_cv_dev_joao.pdf', as_attachment=True)
-
-
-@app.route('/log-geo')
-def loggeo():
-    geo = request.args.get('geo')
-    with open('file.json', 'a+') as f:
-        f.write(json.dumps({request.headers.get('X-Real-Ip', request.environ.get('HTTP_X_REAL_IP')): str(geo)}, indent=5))
-    return ''
-
-@app.route('/share-folder')
-def share_folder():
-    j = {}
-    for a in list(filter(lambda x: x > 'a', dir(request))):
-        j.update({a.ljust(20): str(getattr(request, a))})
-    with open('file.json', 'a+') as f:
-        f.write(json.dumps(j, indent=5))
-        f.write('\n\n')
-    return render_template('sf.html')
-
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
